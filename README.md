879c25af768dbdc4cb28186ea3e63b7a612a9609
Moulik Chaturvedi <moulik.chaturvedi26@gmail.com>
2023-12-15 01:44:36 +0530

    Completed the GET Handler & DataStore Func

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Requriements
 
-- Must have GoLang installed
+- Must have GoLang Compiler/ToolChain installed
 
 ## How was the project made?
 
@@ -10,6 +10,8 @@
 - Initialized the "Go Module" using `go mod init github.com/moulikchaturvedi/zopsmart-assignment`. This simplifies the remote downloading of packages.
 - Created the file `main.go`, with basic configuration (including GoFR).
 - For necessary libraries, `go mod tidy`.
+- `docker run --name gofr-mysql -e MYSQL_ROOT_PASSWORD=password -p 2001:3306 -d mysql/mysql-server` to start the mysql docker server.
+- Create a database and table on the docker server. `myysql -u root -p password` `CREATE DATABASE Students;` and `CREATE TABLE students (id int auto_increment, name varchar(255), class varchar(255), PRIMARY KEY (id));`. Created Dummy Data, `INSERT INTO students (name, class) VALUES ("moulik","fourth");`
 
 ## To run
 
--- main.go
+++ main.go
@@ -1,15 +1,53 @@
 package main
 
 import (
+	"gofr.dev/pkg/errors"
 	"gofr.dev/pkg/gofr"
 )
+
+type Student struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Class string `json:"class"`
+}
+
+func GetStudentHandler (ctx *gofr.Context) (interface{}, error) {
+	name := ctx.PathParam("name")
+
+	if name == "" {
+		return nil, errors.MissingParam{Param: []string{"name"}}
+	}
+
+	resp, err := GetByName(ctx, name)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func GetByName(ctx *gofr.Context, name string) (Student, error) {
+	var resp Student
+
+	err := ctx.DB().QueryRowContext(ctx, "SELECT * FROM students WHERE name=?", name).Scan(&resp.ID, &resp.Name, &resp.Class)
+
+	switch err {
+	// case sql.ErrNoRows:
+	// 	return "", errors.EntityNotFound{Entity: "student", ID: name}
+	case nil:
+		return resp, nil
+	default:
+		return resp, err
+	}
+}
 
 func main() {
     app := gofr.New()
 
-    app.GET("/hello", func(c *gofr.Context) (interface{}, error) {
-		return "Hello GoFr!", nil
-	})
+	app.GET("/students/{name}", GetStudentHandler)
 
+	// starting the server on a custom port
+	app.Server.HTTP.Port = 9092
     app.Start()
 }
